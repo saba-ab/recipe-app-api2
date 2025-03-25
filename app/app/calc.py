1064c00af82f731af6a4c64ca1f7e55f481807e8
saba-ab <sabaabjandadze@gmail.com>
2025-03-25 22:20:37 +0400

    feat (testing): implemented two tests for calc module for testing django test functionality itself.

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,7 +1,17 @@
+"""
+This module provides a simple calculator with two functions: add and subtract.
+"""
+
 
 def add(a: int, b: int) -> int:
+    """Add two numbers"""
     return int(a) + int(b)
 
 
 def subtract(a: int, b: int) -> int:
+    """Subtract two numbers"""
     return int(a) - int(b)
+
+def divide(a: int, b: int) -> float:
+    """Divide two numbers"""
+    return float(a) / float(b)
--- app/app/tests.py
+++ app/app/tests.py
@@ -1,18 +1,25 @@
 from django.test import SimpleTestCase
-from app import calc
-
+from app.calc import add, subtract, divide
 
 class CalcTest(SimpleTestCase):
     """Test the calc module."""
 
     def test_add(self):
         """Testing adding numbers"""
-        res = calc.add(1, 2)
+        res = add(1, 2)
         self.assertEqual(res, 3)
 
     def test_subtract(self):
         """Testing subtracting numbers"""
 
-        res = calc.subtract(10, 4)
+        res = subtract(10, 4)
 
         self.assertEqual(res, 6)
+
+    def test_divide(self):
+        """Testing dividing numbers"""
+
+        res = divide(10, 2)
+
+        self.assertEqual(res, 5)
+
